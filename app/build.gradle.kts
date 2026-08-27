03b8b24a3dcae23cd9a10b3c802ff81c72bae5cd
蛮吉 <hanabi.almost@gmail.com>
2026-08-27 15:04:39 +0000

    chore: 迭代版本 2.7.0 (270)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "fuck.andes"
         minSdk = 34
         targetSdk = 36
-        versionCode = 269
-        versionName = "2.6.9"
+        versionCode = 270
+        versionName = "2.7.0"
     }
 
     signingConfigs {
